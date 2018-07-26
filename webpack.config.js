644184b995948af4e5e298fc43f067c48dcdc4e0
Denis Rolya <drolya@passionfive.de>
2018-07-26 14:04:30 +0200

    style(reveal): integrate reveal theme

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,12 @@ Encore
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
     .addEntry('js/main', './assets/js/main.js')
+    .addEntry('js/reveal', './assets/js/reveal.js')
+    .addStyleEntry('css/reveal', './assets/css/reveal.scss')
     .addStyleEntry('css/main', './assets/css/main.scss')
 
     // uncomment if you use Sass/SCSS files

09df99fa19004e3f43fa03d00ac71bb70132cfd6
Denis Rolya <denrolya@gmail.com>
2018-08-05 15:03:20 +0200

    fix(translations form): fix applying 'active' class

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
